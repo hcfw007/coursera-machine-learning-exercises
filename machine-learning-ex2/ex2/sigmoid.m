2c7644f48301315dd058bab5d66e6e81de73e605
NickWang <hcfw007@126.com>
2019-04-20 20:44:47 +0800

    ex2 warm up

--- machine-learning-ex2/ex2/sigmoid.m
+++ machine-learning-ex2/ex2/sigmoid.m
@@ -3,14 +3,14 @@ function g = sigmoid(z)
 %   g = SIGMOID(z) computes the sigmoid of z.
 
 % You need to return the following variables correctly 
-g = zeros(size(z));
+g = ones(size(z));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
 
-
+g = g ./ (exp(z * -1) + 1);
 
 
 % =============================================================
