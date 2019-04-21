a1b09b9d615ad7e3e9262be3ff2cc8388a9e7fa2
NickWang <hcfw007@126.com>
2019-04-21 22:04:59 +0800

    ex2 gradient

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -23,7 +23,7 @@ grad = zeros(size(theta));
 
 h = sigmoid(X * theta); 
 J = (1 / m) * sum(- y .* log(h) - (1 - y) .* log(1 - h));
-grad = (1 / m) * sum(h - y) .* X;
+grad = (1 / m) * X' * (h - y);
 
 
 
